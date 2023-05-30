c44c0243e1a8484ef8ffe53cff8cc0fe7020ef1d
HaiBV <buivuhai.bk@gmail.com>
2023-05-30 23:27:12 +0700

    feat: add init list players and redux

---
+++ client-site/src/data/players.ts
@@ -0,0 +1,29 @@
+const initPlayers = [
+  {
+    id: 1,
+    name: "Captain America",
+    score: 42,
+  },
+  {
+    id: 2,
+    name: "Iron man",
+    score: 36,
+  },
+  {
+    id: 3,
+    name: "Spidey",
+    score: 17,
+  },
+  {
+    id: 4,
+    name: "Captain Marvel",
+    score: 28,
+  },
+  {
+    id: 5,
+    name: "Hulk",
+    score: 29,
+  },
+];
+
+export default initPlayers;
--- client-site/src/pages/ScoreBoard/ScoreBoard.tsx
+++ client-site/src/pages/ScoreBoard/ScoreBoard.tsx
@@ -2,20 +2,43 @@ import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import "./scoreboard.css";
+import initPlayers from "../../data/players";
+
+interface Player {
+	id: number;
+  name: string;
+  score: number;
+}
 
 const ScoreBoard: React.FC = () => {
+  const [players, setPlayers] = useState<Player[]>(initPlayers);
   const [playerScore, setPlayerScore] = useState(0);
 
   return (
-    <Container className="scoreboard">
-      <Row>My Scoreboard</Row>
-      <Row>
-				<Col>Player name</Col>
-				<Col><button onClick={() => setPlayerScore(playerScore + 1)}>+</button></Col>
-				<Col>{playerScore}</Col>
-				<Col><button onClick={() => setPlayerScore(playerScore - 1)}>-</button></Col>
-			</Row>
+    <Container>
+      <section className="scoreboard">
+        <Container fluid>
+          <Row className="header">My Scoreboard</Row>
+          <div className="players">
+            {players.map((player) => (
+              <Row key={player.id}>
+                <Col md={8}>{player.name}</Col>
+                <Col md={4} className="player-score">
+                  <Button onClick={() => setPlayerScore(playerScore - 1)} className="counter-action decrement btn-success btn btn-default">
+                    -
+                  </Button>
+                  {player.score}
+                  <Button onClick={() => setPlayerScore(playerScore + 1)} className="counter-action increment btn-danger btn btn-default">
+                    +
+                  </Button>
+                </Col>
+              </Row>
+            ))}
+          </div>
+        </Container>
+      </section>
     </Container>
   );
 };
